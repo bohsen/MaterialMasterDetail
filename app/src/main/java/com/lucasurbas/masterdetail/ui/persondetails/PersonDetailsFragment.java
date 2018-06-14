b40a750a5e74c7e17d839851a01548dd148acc50
Thomas Schmidt <bohsen@gmail.com>
2018-06-14 15:03:24 +0200

    Refactored person detail layout. It now uses a coordinatorlayout, AppBarLayout and a ConstraintLayout inside a NestedScroolview to display person details.

--- app/src/main/java/com/lucasurbas/masterdetail/ui/persondetails/PersonDetailsFragment.java
+++ app/src/main/java/com/lucasurbas/masterdetail/ui/persondetails/PersonDetailsFragment.java
@@ -75,6 +75,7 @@ public class PersonDetailsFragment extends Fragment {
 
     private void setPerson(Person person) {
         toolbar.setTitle(person.getName());
+        toolbar.setSubtitle(person.getName());
         description.setText(person.getDescription());
     }
 }
