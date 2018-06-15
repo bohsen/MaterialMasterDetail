6670a8213efaac5a64a68754d458fb108189fc3d
Thomas Schmidt <bohsen@gmail.com>
2018-06-15 15:04:07 +0200

    Added PersonDetailView class.

---
+++ app/src/main/java/com/lucasurbas/masterdetail/ui/people/PersonDetailView.kt
@@ -0,0 +1,12 @@
+package com.lucasurbas.masterdetail.ui.people
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+class PersonDetailView(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int)
+    : View(context, attributeSet, defStyleAttr) {
+
+
+}
