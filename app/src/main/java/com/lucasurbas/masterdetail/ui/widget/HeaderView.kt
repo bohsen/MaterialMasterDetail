f73348beb96ff7d23d0b246ef774d283fb51dfe5
Thomas Schmidt <bohsen@gmail.com>
2018-09-10 14:33:41 +0200

    Refactored PersonDetails UI to allow editing of patient height, weight and study priority.

--- app/src/main/java/com/lucasurbas/masterdetail/ui/widget/HeaderView.kt
+++ app/src/main/java/com/lucasurbas/masterdetail/ui/widget/HeaderView.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.lucasurbas.masterdetail.R
+import kotlinx.android.synthetic.main.custom_headerview.view.custom_headerview_study_priority
 
 
 class HeaderView : ConstraintLayout {
@@ -16,6 +19,28 @@ class HeaderView : ConstraintLayout {
             R.layout.custom_headerview,
             this,
             true)
+
+        custom_headerview_study_priority.apply {
+            val spinnerAdapter = ArrayAdapter.createFromResource(context, R.array.study_priority, R.layout.custom_headerview_spinner_item_layout)
+            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+            adapter = spinnerAdapter
+        }
+
+        custom_headerview_study_priority.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+        }
     }
 
     constructor(context: Context) : super(context)
